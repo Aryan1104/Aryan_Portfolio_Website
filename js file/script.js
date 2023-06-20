52934b2003505bac7d18a4a2a036cd33a0dd4956
Aryan Mishra <123317319+Aryan1104@users.noreply.github.com>
2023-06-21 01:49:25 +0530

    Add files via upload

---
+++ js file/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Full-Stack Web Developer", "Blogger", "Student"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backbackDelay: 1000,
+    loop: true
+})
